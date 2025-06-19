c731fb7f08c93412b264d22fbfcd3900a584dd99
P1POX <xxxtentacionpipo@gmail.com>
2025-06-18 22:40:57 -0400

    Conexion a base de datos

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-requiere('dotenv').config();
+require('dotenv').config();
 
 module.exports = {
     app: {
@@ -7,7 +7,7 @@ module.exports = {
     mysql: {
         host: process.env.MYSQL_HOST || "localhost",
         user: process.env.MYSQL_USER || "root",
-        password: process.env.MYSQL_PASS || "",
+        password: process.env.MYSQL_PASSWORD || "",
         database: process.env.MYSQL_DATABASE || "backend"
     }
 }
