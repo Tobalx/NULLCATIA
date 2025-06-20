be60c664b86d14a737d174efa98a7fd4628ac032
mariv542 <marielvy542@gmail.com>
2025-06-20 15:11:05 -0400

    Swagger implementado

--- src/index.js
+++ src/index.js
@@ -3,22 +3,33 @@ const routes = require('./routes/routes.js')
 const morgan = require('morgan');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const app = express();
 const port = 5000;
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
 //middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 //configuracionejs y layaut
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 app.set('layout', 'layout');
 app.use(express.static(__dirname + '/public'));
+
 //bootstrap desde node_module
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
 //rutas
 app.use(routes);
 
